d226edb8eb190ea158572a0de8aa632709055766
Andrii Lytvyn <indusfreelancer@gmail.com>
2020-04-28 10:51:44 +0300

    Більш детальний опис документа, повна анотація)

--- app/Document.php
+++ app/Document.php
@@ -3,9 +3,22 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Document extends Model
 {
     protected $table = 'document';
     protected $primaryKey = 'doc_id';
+
+public function getAnnotationAttribute()
+{
+    $annot = $this->annot1.$this->annot2.$this->annot3.$this->annot4;
+    $fragments = DB::table('doc_annotation')->where('doc_id', $this->doc_id )->get();
+    foreach ($fragments as $fragment ){
+        $annot.=$fragment->fragment;
+    }
+
+
+    return $annot;
+}
 }
--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -8,38 +8,50 @@
                     <div class="card-header">{{$document->name}}</div>
 
                     <div class="card-body">
-                        <b>Вид документа:</b>&nbsp;{{$document->doc_type}}<br>
+                        <p>
+                            <b>Вид документа:</b>&nbsp;{{$document->doc_type}}<br>
 
 
 
 
-                        <b>Рік публікації:</b> {{$document->publ_year}}<br>
-                        <b>Місце публікації:</b> {{$document->publ_place}}<br>
+                            <b>Рік публікації:</b> {{$document->publ_year}}<br>
+                            <b>Місце публікації:</b> {{$document->publ_place}}<br>
 
-                        <hr>
-                        <b>Publisher:</b> {{$document->publisher}}<br><br>
-                        <hr>
+                            <hr>
+                            <b>Publisher:</b> {{$document->publisher}}<br><br>
+                            <hr>
 
-                        <b>Автор:</b>&nbsp;{{$document->author}}&nbsp;<br>
-                        <b>Авторський знак:</b>&nbsp;{{$document->author_mark}}&nbsp;<br>
-                        <b>Вид автора:</b>&nbsp;{{$document->author_type}}<br>
+                            <b>Автор:</b>&nbsp;{{$document->author}}&nbsp;<br>
+                            <b>Авторський знак:</b>&nbsp;{{$document->author_mark}}&nbsp;<br>
+                            <b>Вид автора:</b>&nbsp;{{$document->author_type}}<br>
 
-                        <b>Мова:</b>&nbsp;{{$document->lang_kod}}&nbsp;{{$document->lang}}&nbsp;<br>
-                        <b>Обсяг:</b>&nbsp;{{$document->sizem}}<br>
+                            <b>Мова:</b>&nbsp;{{$document->lang_kod}}&nbsp;{{$document->lang}}&nbsp;<br>
+                            <b>Обсяг:</b>&nbsp;{{$document->sizem}}<br>
 
 
 
-                        <b>Шифр:</b>&nbsp;{{$document->cipher}}<br>
-                        <hr>
-                        <b>УДК:</b>&nbsp;{{$document->udk}}<br>
-                        <hr>
-                        <b>ББК:</b>&nbsp;{{$document->bbk}}<br>
-                        <hr>
-                        <b>ISBN:</b>&nbsp;{{$document->isbn}}<br>
+                            <b>Шифр:</b>&nbsp;{{$document->cipher}}<br>
+                            <hr>
+                            <b>УДК:</b>&nbsp;{{$document->udk}}<br>
+                            <hr>
+                            <b>ББК:</b>&nbsp;{{$document->bbk}}<br>
+                            <hr>
+                            <b>ISBN:</b>&nbsp;{{$document->isbn}}<br>
 
-                        <hr>
-                        <b>ISSN:</b>&nbsp;{{$document->issn}}<br>
-                        <hr>
+                            <hr>
+                            <b>ISSN:</b>&nbsp;{{$document->issn}}<br>
+                            <hr>
+                        </p>
+                        <p>
+                            <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                                Аннотацiя:
+                            </a>
+                        </p>
+                        <div class="collapse" id="collapseExample">
+                            <div class="card card-body">
+                                {{$document->Annotation}}
+                            </div>
+                        </div>
 
                     </div>
                 </div>
