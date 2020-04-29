9315a765c54f69c8f2527726182b029e53a9cf8f
Andrii Lytvyn <andrii.lytvyn@mail.ee>
2020-04-29 14:08:20 +0300

    Update book.blade.php

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -10,17 +10,17 @@
                         @isset($document->doc_type)<b>Вид документа:</b>&nbsp;{{$document->doc_type}}<br>@endisset
                         @isset($document->publ_year)<b>Рік публікації:</b> {{$document->publ_year}}<br>@endisset
                         @isset($document->publ_place)<b>Місце публікації:</b> {{$document->publ_place}}<br>@endisset
-                        @isset($document->publisher)<hr><b>Publisher:</b> {{$document->publisher}}<br><br>@endisset
+                        @isset($document->publisher)<hr><b>Видавництво:</b> {{$document->publisher}}<br><br>@endisset
                         @isset($document->author)<hr><b>Автор:</b>&nbsp;{{$document->author}}&nbsp;<br>@endisset
                         @isset($document->author_mark)<b>Авторський знак:</b>&nbsp;{{$document->author_mark}}&nbsp;<br>@endisset
                         @isset($document->author_type)<b>Вид автора:</b>&nbsp;{{$document->author_type}}<br>@endisset
                         @isset($document->lang_kod)<b>Мова:</b>&nbsp;{{$document->lang_kod}}&nbsp;{{$document->lang}}&nbsp;<br>@endisset
                         @isset($document->sizem)<b>Обсяг:</b>&nbsp;{{$document->sizem}}<br>@endisset
                         @isset($document->cipher)<b>Шифр:</b>&nbsp;{{$document->cipher}}<br>@endisset
-                        @isset($document->udk)<hr><b>УДК:</b>&nbsp;{{$document->udk}}<br>@endisset
-                        @isset($document->bbk)<hr><b>ББК:</b>&nbsp;{{$document->bbk}}<br>@endisset
-                        @isset($document->isbn)<hr><b>ISBN:</b>&nbsp;{{$document->isbn}}<br>@endisset
-                        @isset($document->issn)<hr><b>ISSN:</b>&nbsp;{{$document->issn}}<br>@endisset
+                        @isset($document->udk)<b>УДК:</b>&nbsp;{{$document->udk}}<br>@endisset
+                        @isset($document->bbk)<b>ББК:</b>&nbsp;{{$document->bbk}}<br>@endisset
+                        @isset($document->isbn)<b>ISBN:</b>&nbsp;{{$document->isbn}}<br>@endisset
+                        @isset($document->issn)<b>ISSN:</b>&nbsp;{{$document->issn}}<br>@endisset
                         @isset($document->annotation)
                                 <hr>
                                 <p>
